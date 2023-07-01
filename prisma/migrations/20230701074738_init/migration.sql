09e42630a780358879011995903ca28af9085646
Salman M <salmanmoh.eth@gmail.com>
2023-07-01 10:48:42 +0300

    fix schema

--- prisma/migrations/20230701074738_init/migration.sql
+++ prisma/migrations/20230701074738_init/migration.sql
@@ -25,13 +25,12 @@ CREATE TABLE `Post` (
     `updatedAt` DATETIME(3) NOT NULL,
 
     INDEX `Post_title_idx`(`title`),
-    UNIQUE INDEX `Post_title_authorId_key`(`title`, `authorId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Category` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `Category_name_key`(`name`),
@@ -40,12 +39,21 @@ CREATE TABLE `Category` (
 
 -- CreateTable
 CREATE TABLE `UserPreference` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `emailUpdates` BOOLEAN NOT NULL DEFAULT false,
     `userId` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `UserPreference_userId_key`(`userId`),
     PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_CategoryToPost` (
+    `A` INTEGER NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_CategoryToPost_AB_unique`(`A`, `B`),
+    INDEX `_CategoryToPost_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
@@ -56,3 +64,9 @@ ALTER TABLE `Post` ADD CONSTRAINT `Post_likerId_fkey` FOREIGN KEY (`likerId`) RE
 
 -- AddForeignKey
 ALTER TABLE `UserPreference` ADD CONSTRAINT `UserPreference_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_CategoryToPost` ADD CONSTRAINT `_CategoryToPost_A_fkey` FOREIGN KEY (`A`) REFERENCES `Category`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_CategoryToPost` ADD CONSTRAINT `_CategoryToPost_B_fkey` FOREIGN KEY (`B`) REFERENCES `Post`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
